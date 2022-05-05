3a3266b970098941ba847fcc0356557762812842
vitoormt <84139448+vitoormt@users.noreply.github.com>
2022-05-05 09:30:26 -0300

    Add files via upload

--- routes/gallery.js
+++ routes/gallery.js
@@ -43,4 +43,40 @@ module.exports = (app) => {
         })
 
     })
+    //visualizar a imagem que será alterada
+    app.get('/alterar_gallery',async(req,res)=>{
+        //recuperar o id da barra de endereço
+        var id = req.query.id
+        //procurar um decomento com o id
+        var procurar = await gallery.findOne({_id:id})
+        //exebir a imagem localizada
+        res.render('gallery_alterar.ejs', {dados:procurar})
+    })
+
+    //alterar a imagem selecionada
+    app.post('/alterar_gallery', (req, res) => {
+        //executar o upload da imagem
+        upload(req, res, async(err) => {
+            if (err instanceof multer.MulterError) {
+                //res.send("Arquivo maior que o limite")
+                res.render('erros.ejs', { erro: "Arquivo maior que o limite!" })
+            } else if (err) {
+                //res.send("Tipo de arquivo inválido")
+                res.render('erros.ejs', { erro: "Tipo de Arquivo Inválido" })
+            } else {
+                //conectar com o databaase
+                conexao()
+                    //gravar o nome do arquivo na collection gallery
+                var arquivo = await gallery.findOneAndUpdate(
+                    {_id:req.query.id},
+                    {
+                        arquivo: req.file.filename
+                    })
+                    //apos o upload voltar para o formulario
+                res.redirect('/gallery')
+            }
+        })
+
+    })
+
 }
